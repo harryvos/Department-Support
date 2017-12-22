3286dc893c97b65b969ac054d3c32c049d7b2f18
Harry Vos <studio@harryvos.com>
2017-12-22 15:12:53 +0000

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,33 @@
+var express = require('express')
+var router = express.Router()
+
+// Route index page
+router.get('/', function (req, res) {
+  res.render('index')
+})
+
+router.get('/cs-check', function (req, res) {
+  res.render('/cs-check')
+})
+
+// add your routes here
+
+router.post('/cs-stage', function (req, res) {
+
+  // Make a variable and give it the value from 'juggling-balls'
+  var civilServant = req.session.data['cs-check']
+
+  // Check whether the variable matches a condition
+  if (civilServant == "cs"){
+    // Send user to next page
+    res.redirect('/cs-stage')
+  }
+  else {
+    // Send user to ineligible page
+    res.redirect('/non-cs-stage')
+  }
+
+})
+
+
+module.exports = router
